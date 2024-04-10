05915a5aeb69002db28da830e52cd93e2adf235f
Mradul Singhal <singhalmradul@gmail.com>
2024-04-10 09:40:12 -0400

    allow headers `Origin`, `X-Requested-With`, `Content-Type` and `Accept`

--- src/main/java/io/github/singhalmradul/reverseproxy/filters/LoggingFilter.java
+++ src/main/java/io/github/singhalmradul/reverseproxy/filters/LoggingFilter.java
@@ -20,18 +20,23 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class LoggingFilter implements GlobalFilter {
+
     Logger log = LoggerFactory.getLogger(getClass());
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, emptySet());
         String originalUri = uris.isEmpty()
-                ? exchange.getRequest().getURI().toString()
-                : uris.iterator().next().toString();
+            ? exchange.getRequest().getURI().toString()
+            : uris.iterator().next().toString();
         Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
         URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
-        log.info("\033[0;33mIncoming request \033[4;34m" + originalUri + "\033[0;33m is routed to id: \033[1;35m" + route.getId()
-                + "\033[0;33m, uri: \033[4;34m" + routeUri + "\033[0m");
+        log.info(
+            "\033[0;33mIncoming request \033[4;34m{}\033[0;33m is routed to id: \033[1;35m{}\033[0;33m, uri: \033[4;34m{}\033[0m",
+            originalUri,
+            route.getId(),
+            routeUri
+        );
         return chain.filter(exchange);
     }
 }
